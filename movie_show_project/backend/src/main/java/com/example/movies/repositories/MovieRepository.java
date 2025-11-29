eb6a926b9d3671654c3350c7698e3e9c60129f3b
bhavanalalam <bhavanalalam4@gmail.com>
2025-11-29 20:17:01 +0530

    Delete movie_show_project directory

--- movie_show_project/backend/src/main/java/com/example/movies/repositories/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.movies.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.movies.entities.Movie;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
